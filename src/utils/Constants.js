f59e5d328ab396ad32394f57c260cc0f4754dfd0
Gautam Krishnan Chittaranjana <cgkrshnn@gmail.com>
2020-09-27 00:22:28 +0800

    Basic skeleton app setup completed

---
+++ src/utils/Constants.js
@@ -0,0 +1,18 @@
+//define all constants here
+
+const INFURA_URL = 'https://mainnet.infura.io/v3/';
+const PROJECT_ID = 'ec0d7f0287f04925a468ce1a574dc30a';
+const URL = INFURA_URL+PROJECT_ID;
+const DEFAULT_ADDRESS = '0xaEdd8BC2c9AbC2F7887fF4bbb33cA00874dB05A5';
+
+const METHOD_CURRENT_BLOCK = 'eth_blockNumber';
+const METHOD_ETHER_BALANCE = 'eth_getBalance';
+
+export {
+    INFURA_URL,
+    PROJECT_ID,
+    URL,
+    DEFAULT_ADDRESS,
+    METHOD_CURRENT_BLOCK,
+    METHOD_ETHER_BALANCE,
+};
