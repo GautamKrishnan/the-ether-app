0ab492e98cb5e5196cc6a7848ee05ace9e0eafd6
Gautam Krishnan Chittaranjana <cgkrshnn@gmail.com>
2020-09-26 19:10:10 +0800

    Updates

---
+++ precache-manifest.4770ff0838594c6e97de9f9a1bf29b69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d11ed9431d2290f62e73a5ad00af93",
+    "url": "/the-ether-app/index.html"
+  },
+  {
+    "revision": "bc030ed9ba0018ad008c",
+    "url": "/the-ether-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a062df807ed435894d2b",
+    "url": "/the-ether-app/static/js/2.1e707960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-ether-app/static/js/2.1e707960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc030ed9ba0018ad008c",
+    "url": "/the-ether-app/static/js/main.abd4f3dd.chunk.js"
+  },
+  {
+    "revision": "778cf15cfe41261d7749",
+    "url": "/the-ether-app/static/js/runtime-main.7938491c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-ether-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
