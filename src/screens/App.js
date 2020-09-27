22bdcbc04d5b470426bf8c4a9f83baf38ca3c1a7
Gautam Krishnan Chittaranjana <cgkrshnn@gmail.com>
2020-09-27 10:54:05 +0800

    Basic UI implemented

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,8 +1,9 @@
 import React , {useState, useEffect} from 'react';
-import logo from '../logo.svg';
 import '../styles/App.css';
 import {getCurrentBalance, getCurrentBlock} from '../api/EthereumAPI';
 import * as Constants from '../utils/Constants'
+
+const ETHEREUM_LOGO = require('../assets/Ethereum_Classic_Logo.svg');
 
 function App() {
   const [contractAddress,setcntAddress] = useState(Constants.DEFAULT_ADDRESS);
@@ -16,27 +17,39 @@ function App() {
               getCurrentBalance(contractAddress),
               getCurrentBlock(),
           ]);
-          setCurrentBalance( parseInt(balance, 16));
+          setCurrentBalance( (parseInt(balance, 16) * Math.pow(10,(-18))).toFixed(9));
           setCurrentBlockHeight( parseInt(block, 16));
       })();
   },[]);
 
-  console.log(currentBalance);
-  console.log(currentBlockHeight);
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        The address being checked is {contractAddress}
-        </p>
-        <p>
-         whose current ether balance is {currentBalance}
-        </p>
-        <p>
-         The current Ethereum block height is {currentBlockHeight}
-        </p>
+        <img src={ETHEREUM_LOGO} className="App-logo" alt="logo" />
+        <div className="Div-text">
+            <p className="Align-left">
+                The address being checked is
+            </p>
+            <p className="Align-right">
+                {contractAddress}
+            </p>
+        </div>
+        <div className="Div-text">
+            <p className="Align-left">
+                whose current Ether balance is
+            </p>
+            <p className="Align-right">
+                {currentBalance}
+            </p>
+        </div>
+        <div className="Div-text">
+            <p className="Align-left">
+                The current Ethereum block height is
+            </p>
+            <p className="Align-right">
+                {currentBlockHeight}
+            </p>
+        </div>
       </header>
     </div>
   );
