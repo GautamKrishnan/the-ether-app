ce1d983c7c077a0a6f61264f095457ce43c204f3
Gautam Krishnan Chittaranjana <cgkrshnn@gmail.com>
2020-09-27 11:05:10 +0800

    Revert "Updates"
    This reverts commit b94a4fa90931ae55c4aa485cbd863b5ee26a1911.

---
+++ precache-manifest.8a341e2f09e446482617ddf2621a3cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840f6d0d23c88dacfa7cdc6e80e8c95e",
+    "url": "/the-ether-app/index.html"
+  },
+  {
+    "revision": "a13bf489142bdc567f6f",
+    "url": "/the-ether-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd1a15ce4f2878b0a144",
+    "url": "/the-ether-app/static/js/2.ec4f5533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-ether-app/static/js/2.ec4f5533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13bf489142bdc567f6f",
+    "url": "/the-ether-app/static/js/main.44d95613.chunk.js"
+  },
+  {
+    "revision": "778cf15cfe41261d7749",
+    "url": "/the-ether-app/static/js/runtime-main.7938491c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-ether-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
